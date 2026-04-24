ba527a8a06ad2a8d9c4e86d3b157caba9f7e220a
Max Pfeiffer <max@maxpfeiffer.ch>
2026-04-24 14:11:28 +0200

    First working version of Dockerfile

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Windrose Dedicated Server - Docker Image and Helm Chart
-
-
+Start the Windrose server:
+```shell
+ docker run -it --user windrose --publish 28050:28050/tcp --publish 28050:28050/udp windrose:test
+```
 
 ## Additional Information Sources
 * [SteamDB](https://steamdb.info/app/4129620/info/)
 * [Official Windrose Dedicated Server Guide](https://playwindrose.com/dedicated-server-guide)
+* https://developer.valvesoftware.com/wiki/SteamCMD
 
 ## Other Game Server Projects
 * [Rust dedicated server](https://github.com/max-pfeiffer/rust-game-server-docker)
--- build/start.sh
+++ build/start.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-wine /srv/windrose/WindroseServer.exe
+SERVER_EXECUTABLE=/srv/windrose/R5/Binaries/Win64/WindroseServer-Win64-Shipping.exe
+SERVER_DESCRIPTION=/srv/windrose/R5/ServerDescription.json
+COUNTER=0
+
+echo "Starting Windrose Dedicated Server"
+if [ ! -f "$SERVER_DESCRIPTION" ]; then
+  echo "Creating ServerDescription.json ..."
+  xvfb-run --auto-servernum wine "$SERVER_EXECUTABLE" -log -STDOUT >/dev/null 2>&1 &
+  RUN_PID=$!
+
+  while [ ! -f "$SERVER_DESCRIPTION" ] && [ $COUNTER -lt 120 ]; do
+      sleep 1
+      COUNTER=$((COUNTER + 1))
+  done
+
+  if [ ! -f "$SERVER_DESCRIPTION" ]; then
+    echo "Error: Failed to generate ServerDescription.json"
+    kill "$RUN_PID" 2>/dev/null
+    wait "$RUN_PID" 2>/dev/null
+    wineserver -k 2>/dev/null
+    exit 1
+  fi
+
+  kill "$RUN_PID" 2>/dev/null
+  wait "$RUN_PID" 2>/dev/null
+  wineserver -k 2>/dev/null
+  sleep 2
+  echo "ServerDescription.json created"
+fi
+
+echo "Starting the server ..."
+xvfb-run --auto-servernum wine "$SERVER_EXECUTABLE" -log
